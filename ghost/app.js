9187adc001ffc9da28ec9e2e3c420a03a5e99088
Ghost12G <ghostco694@gmail.com>
2026-09-19 00:28:37 -0500

    calculadora

--- ghost/app.js
+++ ghost/app.js
@@ -1,14 +1,49 @@
-let msj = document.getElementById("Resultado");
+/* DIGITO INGRESADO EN INPUT */
 
-function farengei() {
-    let input = document.getElementById("calculo").value;
-    let formula = (input * 1.8) + 32
-    msj.textContent = formula
+function numero(n) {
+    document.getElementById("resultado").value += n;
+}
+
+/* OPERADORES */
+
+function suma() {
+    document.getElementById("resultado").value += " + ";
+}
+
+function resta() {
+    document.getElementById("resultado").value += " - ";
+}
+
+function multiplicar() {
+    document.getElementById("resultado").value += " * ";
+}
+
+function dividir() {
+    document.getElementById("resultado").value += " / ";
+
 }
 
 
-function celsius() {
-    let input = document.getElementById("calculo").value;
-    let formula = (input - 32) / 1.8
-    msj.textContent = formula.toFixed(4);
+/* CALCULO */
+function calcular() {
+    let resultado = document.getElementById("resultado").value;
+
+    document.getElementById("resultado").value = eval(resultado)
+
+    if (resultado.includes("/ 0")) {
+        document.getElementById("resultado").value = "No se puede dividir";
+        return
+    }
+
+    if (resultado.includes(" / ")) {
+        document.getElementById("resultado").value = eval(resultado).toFixed(2);
+    } else {
+        document.getElementById("resultado").value = eval(resultado)
+    }
+
+}
+
+/* LIMPIAR INPUT */
+function limpiar() {
+    document.getElementById("resultado").value = ""
 }
